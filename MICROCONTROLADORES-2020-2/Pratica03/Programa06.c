5248f7b1c1ea7538910a201229e4cf0fe7bc2fef
rehfalconi <70268719+rehfalconi@users.noreply.github.com>
2020-09-02 10:51:26 -0300

    Create Programa06.c

---
+++ MICROCONTROLADORES-2020-2/Pratica03/Programa06.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(void){
+  int temperaturas [3];
+  //ler 3 temperaturas
+    printf("Informe a temperatura 0:");
+    scanf("%i", temperaturas[0]);
+
+    printf("Informe a temperatura 1:");
+    scanf("%i", temperaturas[1]);
+
+    printf("Informe a temperatura 2:");
+    scanf("%i", temperaturas[2]);
+return 0;
+}
