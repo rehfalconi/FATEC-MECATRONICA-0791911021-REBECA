ab4714b7ffc105cbd7bf672188b672b14963ee99
rehfalconi <70268719+rehfalconi@users.noreply.github.com>
2020-09-02 10:10:23 -0300

    Create Programa05.c

---
+++ MICROCONTROLADORES-2020-2/Pratica03/Programa05.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(void){
+int minhaVariavel;
+minhaVariavel = 10;
+printf("Valor da minhaVariavel: %i\n", minhaVariavel); // apresenta o conteúdo
+printf("Valor da minhaVariavel: %i\n", &minhaVariavel); // apresenta o endereço
+return 0;
+}
